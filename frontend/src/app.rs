12f503aa4624a80542d506aa2e874dd3842ab858
Evan Schoening <evan.schoening@gmail.com>
2024-07-02 21:39:28 -0700

    setup of registration page

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,12 +1,15 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
-use dioxus_router::Router;
+use dioxus_router::{Router, Route};
 use fermi::use_init_atom_root;
+
+use crate::prelude::*;
 
 pub fn App(cx: Scope) -> Element {
     use_init_atom_root(cx);
     cx.render(rsx! {
-        Router {Route { to : "some_url", page::Register {}}}
+        Router {
+            Route { to : page::ACCOUNT_REGISTER, page::Register {}}}
     })
 }
--- frontend/src/page.rs
+++ frontend/src/page.rs
@@ -1,1 +1,8 @@
-pub mod register
+pub mod register;
+
+pub use register::Register;
+pub use route::*;
+
+pub mod route {
+    pub const ACCOUNT_REGISTER: &str = "/account/register";
+}
