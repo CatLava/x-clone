4fed711e47af49b23eed5093b5c8d36d4b8a62f7
Evan Schoening <evan.schoening@gmail.com>
2024-07-02 21:09:14 -0700

    initial commit of new project

---
+++ frontend/src/app.rs
@@ -0,0 +1,12 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+use dioxus_router::Router;
+use fermi::use_init_atom_root;
+
+pub fn App(cx: Scope) -> Element {
+    use_init_atom_root(cx);
+    cx.render(rsx! {
+        Router {Route { to : "some_url", page::Register {}}}
+    })
+}
