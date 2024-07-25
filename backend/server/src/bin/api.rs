bec1d60bfe49ac6f353b8e5a96cdc9272c6053f3
Evan Schoening <evan.schoening@gmail.com>
2024-07-25 14:10:55 -0700

    created api server logic for backend

--- backend/server/src/bin/api.rs
+++ backend/server/src/bin/api.rs
@@ -2,7 +2,7 @@ use clap::{command, Parser, Subcommand};
 use std::net::SocketAddr;
 use color_eyre::{Result, Help, eyre::Context};
 
-use tracing::{debug, info};
+use tracing::{debug, info, error};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about=None)]
@@ -52,11 +52,49 @@ async fn run() -> Result<()> {
                 let path = "private_key.base64";
                 std::fs::write(path, key.as_str())?;
                 info!(target: "uchat_server", path=path, "private key saved to disk");
-                info!(target: "uchat_server", "set API_PRIVATE_KEY env variable with content")
+                info!(target: "uchat_server", "set API_PRIVATE_KEY env variable with content");
                 return Ok(());
             }
         }
     }
+
+    debug!(target: "uchat_server", "loading signing keys");
+    let signing_keys = uchat_server::cli::load_keys()?;
+
+    info!(target: "uchat_server", database_url = args.database_url, "connecting to db");
+    let db_pool = uchat_query:: AsyncConnectionPool::new(&args.database_url)
+        .await
+        .with_suggestion(|| " check database URL")
+        .with_suggestion(|| "ensure correct db access rights")
+        .with_suggestion(|| "make sure db exists")?;
+
+    // Create application state
+    let state = uchat_server::AppState {
+        db_pool,
+        signing_keys,
+        rng: uchat_crypto::new_rng(),
+    };
+
+    info!(target: "uchat_server", bind_addr = %args.bind);
+
+    // new router function
+
+    let router = uchat_server::new_router(state);
+
+    let server = axum::Server::try_bind(&args.bind)
+        .wrap_err_with(|| " server initialization error")
+        .with_suggestion(|| "check bind address")
+        .with_suggestion(|| "check if other service using same port");
+
+
+    let server = server.serve(router.into_make_service());
+
+    info!(target: "uchat_server", "listening");
+
+    if let Err(e) = server.await {
+        error!(target: "uchat_server", server_error = %e);
+    }
+
     Ok(())
 
 
