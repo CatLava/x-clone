ab6e30bc92c7d55374f7925c1a5161224040b67c
Evan Schoening <evan.schoening@gmail.com>
2024-08-18 20:55:29 -0700

    addition to the router

--- backend/server/src/router.rs
+++ backend/server/src/router.rs
@@ -1,5 +1,9 @@
 use crate::AppState;
-use axum::{routing::get, Router};
+use axum::{http::HeaderValue, routing::get, Router};
+use hyper::{header::CONTENT_TYPE, Method};
+use tower::ServiceBuilder;
+use tower_http::{cors::CorsLayer, trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, OnResponse, TraceLayer}, LatencyUnit};
+use tracing::Level;
 
 
 pub fn new_router(state: AppState) -> axum::Router {
@@ -9,6 +13,32 @@ pub fn new_router(state: AppState) -> axum::Router {
     let authorized_routes = Router::new();
 
     Router::new()
-    .merge(public_routes)
-    .merge(authorized_routes)
+        .merge(public_routes)
+        .merge(authorized_routes)
+        .layer(
+            // arrange the layers properly
+            ServiceBuilder::new()
+            .layer(
+                TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().include_headers(true))
+                .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_response(DefaultOnResponse::new()
+                    .level(Level::INFO)
+                    .latency_unit(LatencyUnit::Micros)),
+            ),
+        
+        )
+        .layer(
+            // CORS layer allows comms between FE and BE
+            CorsLayer::new()
+                .allow_methods([Method::GET, Method::POST, Method::OPTIONS, Method::PATCH, Method::PUT])
+                .allow_credentials(true)
+                .allow_origin(
+                    std::env::var("FRONTEND_URL")
+                    .unwrap()
+                    .parse::<HeaderValue>()
+                    .unwrap()
+                )
+                .allow_headers([CONTENT_TYPE])
+        )
 }
