a83cbe45757344e4a3587fc9e099faca15261f68
Evan Schoening <evan.schoening@gmail.com>
2024-09-24 21:21:32 -0700

    working on chat box and FE components for the box

--- frontend/src/page.rs
+++ frontend/src/page.rs
@@ -1,6 +1,7 @@
 pub mod register;
 pub mod login;
 pub mod home;
+pub mod new_post;
 
 pub use register::Register;
 pub use login::Login;
@@ -12,7 +13,7 @@ pub mod route {
     pub const ACCOUNT_REGISTER: &str = "/account/register";
     pub const ACCOUNT_LOGIN: &str = "/account/login";
     pub const HOME: &str = "/home";
-    pub const POST_NEW_CHAT: &str = "/post/new_chat"
+    pub const POST_NEW_CHAT: &str = "/post/new_chat";
 
 
 }
